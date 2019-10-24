938355f94f5d794efee95e9d41e86185c658b913
JamaHCS <jama117@hotmail.com>
2019-10-23 21:25:59 -0500

    Compromisos

---
+++ if.js
@@ -0,0 +1,10 @@
+// var a = prompt("Inserta un angulo a");
+// var b = prompt("Inserta un angulo b");
+// var c = prompt("Inserta un angulo c");
+
+const caca = () => {
+  if (a || b || c > 90) {
+    console.log("Hay un angulo mayor a 90");
+    return "si";
+  }
+};
